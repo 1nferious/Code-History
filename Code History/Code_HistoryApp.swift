e23758dc1f93cb40e277a1caef3ab2d728d76b66
ray <56456669+1nferious@users.noreply.github.com>
2022-12-27 14:56:47 -0500

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by 1nferious on 2022-12-27.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
