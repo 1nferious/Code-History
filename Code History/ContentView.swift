7a58c30680e8c4267e0a43ef1ed0b6dc1cae45af
ray <56456669+1nferious@users.noreply.github.com>
2023-01-05 17:25:30 -0500

    Buttons
    adding in the buttons!

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -23,6 +23,48 @@ struct ContentView: View {
                     .bold()
                     .multilineTextAlignment(.leading)
                 Spacer()
+                HStack {
+                    Button {
+                        print("Selected Choice 1")
+                    } label: {
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    }
+                    Button {
+                        print("Selected Choice 2")
+                    } label: {
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    }
+                    Button {
+                        print("Selected Choice 3")
+                    } label: {
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    }
+                    Button {
+                        print("Selected Choice 4")
+                    } label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    }
+                }
             }
             .foregroundColor(.white)
         }
