1343884f9a2865e1aaa6aa85633e13eba1929d47
ray <56456669+1nferious@users.noreply.github.com>
2023-10-09 13:11:27 -0400

    question model

---
+++ Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Code History
+//
+//  Created by Rayhan on 2023-10-09.
+//
+
+import Foundation
+
+struct Question {
+let questionText: String
+let possibleAnswers: [String]
+let correctAnswerIndex: Int
+}
